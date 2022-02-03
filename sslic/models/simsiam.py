12a2f9f14853655cac32764a001795d74c38f8f8
Gergely Papp <gergely.papp@renyi.hu>
2022-02-03 12:26:41 +0000

    2 layer mlp added to simsiam

--- sslic/models/simsiam.py
+++ sslic/models/simsiam.py
@@ -19,19 +19,22 @@ class SimSiam(BaseModel):
             Dimension of bottleneck layer at predict layer, by default 512
     """
 
-    def __init__(self, base_encoder: nn.Module, pred_dim: int = 512, **kwargs):
+    def __init__(self, base_encoder: nn.Module, pred_dim: int = 512, mlp_len=3, **kwargs):
         super(SimSiam, self).__init__(base_encoder, ssl_loss=simsiam_loss(), **kwargs)
         self.pred_dim = pred_dim
 
         # Projector
-        self.projector = nn.Sequential(nn.Linear(self.prev_dim, self.prev_dim, bias=False),
-                                       nn.BatchNorm1d(self.prev_dim),
-                                       nn.ReLU(inplace=True),
-                                       nn.Linear(self.prev_dim, self.prev_dim, bias=False),
-                                       nn.BatchNorm1d(self.prev_dim),
-                                       nn.ReLU(inplace=True),
-                                       nn.Linear(self.prev_dim, self.dim, bias=False),
-                                       nn.BatchNorm1d(self.dim, affine=False))
+        projector_layers = []
+        for _ in range(mlp_len - 1):
+            projector_layers.extend([
+                nn.Linear(self.prev_dim, self.prev_dim, bias=False),
+                nn.BatchNorm1d(self.prev_dim),
+                nn.ReLU(inplace=True)
+            ])
+        projector_layers.extend([
+            nn.Linear(self.prev_dim, self.dim, bias=False), nn.BatchNorm1d(self.dim, affine=False)
+        ])
+        self.projector = nn.Sequential(*projector_layers)
 
         # Predictor
         self.predictor = nn.Sequential(nn.Linear(self.dim, self.pred_dim, bias=False),
@@ -55,14 +58,19 @@ class SimSiam(BaseModel):
 
 
 def simsiam_imagenet() -> nn.Module:
-    return SimSiam(models.resnet50, pred_dim=512, dim=2048, n_classes=1000, zero_init_residual=True)
+    return SimSiam(models.resnet50,
+                   pred_dim=512,
+                   mlp_len=3,
+                   dim=2048,
+                   n_classes=1000,
+                   zero_init_residual=True)
 
 
 def simsiam_cifar10() -> nn.Module:
     from .cifar_resnet import resnet18
-    return SimSiam(resnet18, pred_dim=64, dim=256, n_classes=10)
+    return SimSiam(resnet18, pred_dim=512, mlp_len=2, dim=2048, n_classes=10)
 
 
 def simsiam_cifar100() -> nn.Module:
     from .cifar_resnet import resnet18
-    return SimSiam(resnet18, pred_dim=64, dim=256, n_classes=100)
+    return SimSiam(resnet18, pred_dim=512, mlp_len=2, dim=2048, n_classes=100)
