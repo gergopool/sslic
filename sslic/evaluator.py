d6bb858ef60a4e209b79a309b885e86f3f5bb32a
Gergely Papp <gergely.papp@renyi.hu>
2022-02-04 10:44:39 +0000

    lin eval added to evaluator

--- sslic/evaluator.py
+++ sslic/evaluator.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from typing import Tuple
 
 from .utils import AllReduce
 
@@ -40,6 +41,11 @@ class SnnEvaluator(nn.Module):
         labels = torch.arange(n_classes).repeat_interleave(max_queue_size)
         self.onehot_labels = nn.Parameter(F.one_hot(labels, n_classes).float())
 
+        # Linear eval
+        self.classifier = nn.Linear(dim, n_classes)
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(self.classifier.parameters(), 1e-2, momentum=0.9)
+
     def update(self, batch_x: torch.Tensor, batch_y: torch.Tensor):
         """Save new embeddings in memory along with their classes.
 
@@ -51,6 +57,8 @@ class SnnEvaluator(nn.Module):
             The labels of the embeddings. Note that all y values must
             be less than the number of classes given at initialisation.
         """
+
+        # SNN
         batch_x = batch_x.detach()
         for (x, y) in zip(batch_x, batch_y):
             # FIFO push
@@ -58,6 +66,13 @@ class SnnEvaluator(nn.Module):
                 i = self.mb_indices[y]
                 self.memory_bank[y][i] = x
                 self.mb_indices[y] = (i + 1) % self.max_queue_size
+
+        # Linear Eval
+        y_hat = self.classifier(batch_x)
+        loss = self.criterion(y_hat, batch_y)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
     def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         """Calculate top1 accuracy, given the representations and
@@ -78,12 +93,39 @@ class SnnEvaluator(nn.Module):
 
         # Similarity matrix
         with torch.no_grad():
+
+            # SNN
             train_x = F.normalize(self.memory_bank.view(-1, self.dim), dim=1)
             val_x = F.normalize(x, dim=1)
-            sim_m = F.softmax(val_x @ train_x.T, dim=1) @ self.onehot_labels
+            snn_y_hat = F.softmax(val_x @ train_x.T, dim=1) @ self.onehot_labels
 
-            # Accuracy
-            top5_preds = sim_m.topk(5, dim=1)[1]
+            # Linear Eval
+            lin_y_hat = self.classifier(x)
+
+        metrics = {}
+        metrics['snn_top1'], metrics['snn_top5'] = self._get_acc(snn_y_hat, y)
+        metrics['lin_top1'], metrics['lin_top5'] = self._get_acc(lin_y_hat, y)
+        return metrics
+
+    def _get_acc(self, y_hat: torch.Tensor, y: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """_get_acc
+        Based on the discrete probability distributions it calculates
+        the top1 and top5 accuracies.
+
+        Parameters
+        ----------
+        y_hat : torch.Tensor
+            The distribtuions.
+        y : torch.Tensor
+            True values as long values.
+
+        Returns
+        -------
+        Tuple[torch.Tensor, torch.Tensor]
+            Top1 and Top5 accuracies respectively.
+        """
+        with torch.no_grad():
+            top5_preds = y_hat.topk(5, dim=1)[1]
             top1_preds = top5_preds[:, 0]
             top5_labels = y.repeat_interleave(5).view(-1, 5)
             top5_acc = (top5_preds == top5_labels).any(dim=1).sum() / len(y)
